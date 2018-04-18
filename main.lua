bf102c1096ab6dc58aa7e91692dfd8affca77987
Lee Synkowski <leesynkowski@baltimorebjj.com>
2018-04-18 08:24:58 -0400

    Adding Rectangles, Task 1

--- main.lua
+++ main.lua
@@ -17,6 +17,17 @@ local centerY = display.contentCenterY
 local _W = display.contentWidth
 local _H = display.contentHeight
 
--- Displays App title
-title = display.newText( "File Demo", centerX, centerY, native.systemFontBold, 20 )
-title:setFillColor( 1,1,0 )
+--Task 1: Screen Rectangles
+local graphicsRectangle = display.newRect( 110, 110, 50, 50)
+graphicsRectangle:setFillColor( 0, 1, 0 )
+
+print("_W ",_W)
+print("_H ",_H)
+print("centerX ",centerX)
+print("centerY ",centerY)
+
+--sceneGroup:insert( graphicsRectangle )
+--label
+--local label1 = display.newText("1", rect1X, rect1Y, rectWidth, rectHeight )
+--label1:setFillColor( 0, 0, 0 )
+--sceneGroup:insert( label1 )  
