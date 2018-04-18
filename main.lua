8e91a97ee5c2096a5ea0789e3ee57b61b0cd6581
lsynkow1 <lsynkow1@jhu.edu>
2018-04-18 16:02:23 -0400

    Adding Picker

--- main.lua
+++ main.lua
@@ -6,6 +6,8 @@
 -- Start Debugging
 require('mobdebug').start()
 
+
+local widget = require( "widget" )
 --local composer = require( "composer" )
 
 --composer.gotoScene( "Scenes.FirstType" )
@@ -31,8 +33,42 @@ infoArea:setFillColor( 1, 0, 0 )
 local menuArea = display.newRect( 0, (7 * _H) / 9, _W, (2 * _H) / 9)
 menuArea:setFillColor( 0, 0, 1 )
 
+--Keep this for adding scence structure
 --sceneGroup:insert( graphicsRectangle )
---label
---local label1 = display.newText("1", rect1X, rect1Y, rectWidth, rectHeight )
---label1:setFillColor( 0, 0, 0 )
---sceneGroup:insert( label1 )  
+
+
+ 
+-- Set up the picker wheel columns
+local columnData = 
+{ 
+    { 
+        align = "Left",
+        width = _W / 2,
+        labelPadding = _W/8,
+        startIndex = 1,
+        labels = { "Shoot", "Pull Guard", "Foot Sweep" }
+    }
+}
+ 
+-- Create the widget
+local pickerWheel = widget.newPickerWheel(
+{
+    x = 0, 
+    y = (7 * _H) / 9,
+    columns = columnData,
+    style = "resizable",
+    width = _W,
+    rowHeight = ((2 * _H) / 9)/5,
+    fontSize = 32
+})
+ 
+-- Get the table of current values for all columns
+-- This can be performed on a button tap, timer execution, or other event
+local values = pickerWheel:getValues()
+ 
+-- Get the value for each column in the wheel, by column index
+--local currentStyle = values[1].value
+--local currentColor = values[2].value
+--local currentSize = values[3].value
+ 
+--print( currentStyle, currentColor, currentSize )
